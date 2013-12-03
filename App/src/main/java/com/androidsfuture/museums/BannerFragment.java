9c98bbb528fdc96f81190c43b951ce22d88e6992
Anthony Alexander <antalex7@gmail.com>
2013-12-03 07:32:36 -0500

    Initial commit

---
+++ App/src/main/java/com/androidsfuture/museums/BannerFragment.java
@@ -0,0 +1,20 @@
+package com.androidsfuture.museums;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tony on 12/2/13.
+ */
+public class BannerFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_banner, container, false);
+
+        return view;
+    }
+}
---
+++ App/src/main/java/com/androidsfuture/museums/ButtonFragment.java
@@ -0,0 +1,20 @@
+package com.androidsfuture.museums;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Tony on 12/2/13.
+ */
+public class ButtonFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_buttons, container,false);
+
+        return view;
+    }
+}
